c997ebbce62cf96aeaa41f71d5e75d58ee7733b9
Alexey Plenkin <aveplen@yandex.ru>
2022-06-05 03:39:27 +0300

    refactor token service with new functions

--- src/tetatet/token_service.ts
+++ src/tetatet/token_service.ts
@@ -1,4 +1,12 @@
-import { decryptAesCbc, encryptAesCbc } from "./cryptoutil";
+import {
+  decryptAesCbc,
+  encryptAesCbc,
+  arrayBufferToBase64,
+  base64ToArrayBuffer,
+  utf8ToBase64,
+  base64ToUtf8,
+  arrayBufferToUtf8,
+} from "./cryptoutil";
 
 export default class TokenService {
   private key: CryptoKey;
@@ -23,32 +31,12 @@ export default class TokenService {
 }
 
 function pack(prot: TokenProtected): string {
-  const b64Syn = _arrayBufferToBase64(prot.SynchronizationBytes);
-  const b64Head = btoa(JSON.stringify(prot.Header));
-  const b64Pld = btoa(JSON.stringify(prot.Payload));
-  const b64Sign = _arrayBufferToBase64(prot.SignatureBytes);
+  const b64Syn = arrayBufferToBase64(prot.SynchronizationBytes);
+  const b64Head = utf8ToBase64(JSON.stringify(prot.Header));
+  const b64Pld = utf8ToBase64(JSON.stringify(prot.Payload));
+  const b64Sign = arrayBufferToBase64(prot.SignatureBytes);
 
   return [b64Syn, b64Head, b64Pld, b64Sign].join(".");
-}
-
-function _base64ToArrayBuffer(base64: string): ArrayBuffer {
-  const binary_string = window.atob(base64);
-  const len = binary_string.length;
-  const bytes = new Uint8Array(len);
-  for (let i = 0; i < len; i++) {
-    bytes[i] = binary_string.charCodeAt(i);
-  }
-  return bytes.buffer;
-}
-
-function _arrayBufferToBase64(buffer: ArrayBuffer): string {
-  let binary = "";
-  const bytes = new Uint8Array(buffer);
-  const len = bytes.byteLength;
-  for (let i = 0; i < len; i++) {
-    binary += String.fromCharCode(bytes[i]);
-  }
-  return window.btoa(binary);
 }
 
 function unpack(token: string): TokenProtected {
@@ -57,10 +45,10 @@ function unpack(token: string): TokenProtected {
     throw "token is damaged or of wrong format";
   }
 
-  const syn = _base64ToArrayBuffer(tokenParts[0]);
-  const head = JSON.parse(atob(tokenParts[1]));
-  const plb: Payload = JSON.parse(atob(tokenParts[2]));
-  const sign = _base64ToArrayBuffer(tokenParts[3]);
+  const syn = base64ToArrayBuffer(tokenParts[0]);
+  const head = JSON.parse(base64ToUtf8(tokenParts[1]));
+  const plb: Payload = JSON.parse(base64ToUtf8(tokenParts[2]));
+  const sign = base64ToArrayBuffer(tokenParts[3]);
 
   return {
     SynchronizationBytes: syn,
@@ -75,8 +63,8 @@ async function protect(
   key: CryptoKey,
   iv: ArrayBuffer
 ): Promise<TokenProtected> {
-  const synb = btoa(JSON.stringify(raw.Synchronization));
-  const encsyn = await encryptAesCbc(_base64ToArrayBuffer(synb), key, iv);
+  const synb = utf8ToBase64(JSON.stringify(raw.Synchronization));
+  const encsyn = await encryptAesCbc(base64ToArrayBuffer(synb), key, iv);
 
   return {
     SynchronizationBytes: encsyn,
@@ -93,10 +81,8 @@ async function unprotect(
 ): Promise<TokenRaw> {
   const synb = await decryptAesCbc(prot.SynchronizationBytes, key, iv);
 
-  const syn = atob(_arrayBufferToBase64(synb));
-
   return {
-    Synchronization: JSON.parse(syn),
+    Synchronization: JSON.parse(arrayBufferToUtf8(synb)),
     Header: prot.Header,
     Payload: prot.Payload,
     SignatureBytes: prot.SignatureBytes,
