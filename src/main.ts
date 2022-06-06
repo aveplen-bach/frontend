4c857e9123f6bc44e81b5e91caf9e98e1cba97dc
Alexey Plenkin <aveplen@yandex.ru>
2022-06-06 03:17:36 +0300

    fix store import

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,11 @@
 import { createApp } from "vue";
 import App from "./App.vue";
 import router from "./router";
-import store from "./store";
+import store, { key } from "./store";
 import Camera from "simple-vue-camera";
 
-createApp(App).component("camera", Camera).use(store).use(router).mount("#app");
+createApp(App)
+  .component("camera", Camera)
+  .use(store, key)
+  .use(router)
+  .mount("#app");
