13c90ee9befddbbde7870f8fe4f8bac76c2ed77d
Alexey Plenkin <aveplen@yandex.ru>
2022-06-06 15:58:41 +0300

    implement protected view

---
+++ src/views/ProtectedView.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="content d-flex justify-content-center align-items-center">
+    <div class="card shadow col-6 card-fixed-height">
+      <div class="card-body d-flex justify-content-center align-items-center">
+        <h1 class="text-success" v-if="authenticated">Авторизован</h1>
+        <h1 class="text-danger" v-else>Не авторизован</h1>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script lang="ts" setup>
+import { useStore } from "vuex";
+import { key } from "@/store";
+import { computed } from "@vue/runtime-core";
+
+const store = useStore(key);
+
+const authenticated = computed(() => store.state.protected.isAuthenticated);
+</script>
+
+<style lang="scss" scoped>
+.card-fixed-height {
+  height: 50vh;
+}
+
+.content {
+  min-height: calc(100vh - 150px);
+}
+</style>
