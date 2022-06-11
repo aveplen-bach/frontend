55b407b0ced3b17e38fb6036a7336bb9817d1e3f
Alexey Plenkin <aveplen@yandex.ru>
2022-06-11 23:21:28 +0300

    split dashboard into components

---
+++ src/dashboard/DashboardSettings.vue
@@ -0,0 +1,41 @@
+<template>
+  <div class="row justify-content-between mb-2">
+    <h3 class="col-auto">Параметры</h3>
+  </div>
+
+  <div class="card">
+    <div class="card-body">
+      <h5 class="card-title">Сравнение векторов</h5>
+      <div class="card-text">
+        <form>
+          <div class="mb-3">
+            <label for="exampleInputEmail1" class="form-label"
+              >Пороговое значение расстояния Евклида</label
+            >
+            <input
+              type="text"
+              class="form-control"
+              id="exampleInputEmail1"
+              aria-describedby="emailHelp"
+              v-model="regUsername"
+            />
+          </div>
+          <button
+            type="submit"
+            class="btn btn-success"
+            @click.prevent="register"
+          >
+            Обновить параметры
+          </button>
+        </form>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script lang="ts" setup>
+import { key } from "@/_store";
+import { useStore } from "vuex";
+
+useStore(key);
+</script>
---
+++ src/dashboard/DashboardUsers.vue
@@ -0,0 +1,191 @@
+<template>
+  <div class="row justify-content-between mb-2">
+    <h3 class="col-auto">Пользователи</h3>
+    <div class="col-md-6 d-flex">
+      <input
+        class="form-control me-2"
+        placeholder="Поиск"
+        aria-label="Search"
+        v-model="search"
+      />
+      <button class="btn btn-outline-success">Найти</button>
+    </div>
+  </div>
+
+  <div class="accordion" id="users-accordion-list">
+    <div class="accordion-item">
+      <h2 class="accordion-header" id="accordion-header-add">
+        <button
+          class="accordion-button"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#accordion-add"
+          aria-expanded="false"
+          aria-controls="accordion-add"
+        >
+          Добавить пользователя
+        </button>
+      </h2>
+      <div
+        id="accordion-add"
+        class="accordion-collapse collapse"
+        aria-labelledby="accordion-header-add"
+        data-bs-parent="#users-accordion-list"
+      >
+        <div class="accordion-body">
+          <form>
+            <div class="mb-3">
+              <label for="exampleInputEmail1" class="form-label"
+                >Имя пользователя</label
+              >
+              <input
+                type="text"
+                class="form-control"
+                id="exampleInputEmail1"
+                aria-describedby="emailHelp"
+                v-model="regUsername"
+              />
+            </div>
+            <div class="mb-3">
+              <label for="exampleInputPassword1" class="form-label"
+                >Пароль</label
+              >
+              <input
+                type="password"
+                class="form-control"
+                id="exampleInputPassword11"
+                v-model="regPassword"
+              />
+            </div>
+            <div class="mb-3">
+              <label for="exampleInputPassword1" class="form-label"
+                >Повтор пароля</label
+              >
+              <input
+                type="password"
+                class="form-control"
+                id="exampleInputPassword1"
+                v-model="regRepeat"
+              />
+            </div>
+            <div class="mb-3 form-check">
+              <input
+                type="checkbox"
+                class="form-check-input"
+                id="exampleCheck1"
+                v-model="regAdmin"
+              />
+              <label class="form-check-label" for="exampleCheck1"
+                >Администратор</label
+              >
+            </div>
+            <div class="mb-3">
+              <label for="formFile" class="form-label">Фото</label>
+              <input
+                class="form-control"
+                type="file"
+                id="formFile"
+                ref="regPhotos"
+              />
+            </div>
+            <button
+              type="submit"
+              class="btn btn-success"
+              @click.prevent="register"
+            >
+              Добавить
+            </button>
+          </form>
+        </div>
+      </div>
+    </div>
+
+    <div v-for="user in searchFound" :key="user.id" class="accordion-item">
+      <h2 class="accordion-header" :id="`accordion-header-${user.userId}`">
+        <button
+          class="accordion-button"
+          type="button"
+          data-bs-toggle="collapse"
+          :data-bs-target="`#accordion-${user.userId}`"
+          aria-expanded="false"
+          :aria-controls="`accordion-${user.userId}`"
+        >
+          {{ user.userId }}. {{ user.username }}
+        </button>
+      </h2>
+      <div
+        :id="`accordion-${user.userId}`"
+        class="accordion-collapse collapse"
+        :aria-labelledby="`accordion-header-${user.userId}`"
+        data-bs-parent="#users-accordion-list"
+      >
+        <div class="accordion-body">
+          <table class="table table-light">
+            <tbody>
+              <tr>
+                <th scope="row">Идентификатор</th>
+                <td>{{ user.userId }}</td>
+              </tr>
+              <tr>
+                <th scope="row">Имя пользователя</th>
+                <td>{{ user.username }}</td>
+              </tr>
+              <tr>
+                <th scope="row">Создан</th>
+                <td>{{ user.createdAt }}</td>
+              </tr>
+              <tr>
+                <th scope="row">Изменен</th>
+                <td>{{ user.updatedAt }}</td>
+              </tr>
+              <tr>
+                <th scope="row">Администратор</th>
+                <td>{{ user.admin }}</td>
+              </tr>
+            </tbody>
+          </table>
+          <button class="btn btn-danger">Delete</button>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script lang="ts" setup>
+import { key } from "@/_store";
+import { ref, onMounted, computed, Ref } from "vue";
+import { useStore } from "vuex";
+const store = useStore(key);
+
+// onMounted(() => {
+//   store.dispatch("adminUsers");
+// });
+
+const search = ref("");
+const searchFound = computed(() => store.state.users?.users);
+
+const regUsername = ref("");
+const regPassword = ref("");
+const regRepeat = ref("");
+const regAdmin = ref(false);
+const regPhotos: Ref = ref(null);
+
+const register = async () => {
+  const fr = new FileReader();
+  fr.onload = (e: ProgressEvent<FileReader>) => {
+    store.dispatch("adminRegister", {
+      username: regUsername.value,
+      password: regPassword.value,
+      repeat: regRepeat.value,
+      admin: regAdmin.value,
+      photo: e.target?.result,
+    });
+  };
+
+  try {
+    fr.readAsArrayBuffer(regPhotos.value.files[0]);
+  } catch (e) {
+    store.commit("DASHBOARD_SET_ERROR", { error: "необходимо выбрать файл" });
+  }
+};
+</script>
