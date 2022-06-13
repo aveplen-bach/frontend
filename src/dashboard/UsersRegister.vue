57c1a93511a4fc13b35ec11167145228eb5b77e6
Alexey Plenkin <aveplen@yandex.ru>
2022-06-14 00:53:32 +0300

    implement register form script

--- src/dashboard/UsersRegister.vue
+++ src/dashboard/UsersRegister.vue
@@ -29,7 +29,7 @@
               class="form-control"
               id="exampleInputEmail1"
               aria-describedby="emailHelp"
-              v-model="regUsername"
+              v-model="username"
             />
           </div>
           <div class="mb-3">
@@ -38,7 +38,7 @@
               type="password"
               class="form-control"
               id="exampleInputPassword11"
-              v-model="regPassword"
+              v-model="password"
             />
           </div>
           <div class="mb-3">
@@ -49,7 +49,7 @@
               type="password"
               class="form-control"
               id="exampleInputPassword1"
-              v-model="regRepeat"
+              v-model="repeat"
             />
           </div>
           <div class="mb-3 form-check">
@@ -57,7 +57,7 @@
               type="checkbox"
               class="form-check-input"
               id="exampleCheck1"
-              v-model="regAdmin"
+              v-model="admin"
             />
             <label class="form-check-label" for="exampleCheck1"
               >Администратор</label
@@ -65,24 +65,70 @@
           </div>
           <div class="mb-3">
             <label for="formFile" class="form-label">Фото</label>
-            <input
-              class="form-control"
-              type="file"
-              id="formFile"
-              ref="regPhotos"
-            />
+            <input class="form-control" type="file" id="formFile" ref="photo" />
           </div>
           <button
             type="submit"
             class="btn btn-success"
+            :class="{ disabled: isSending }"
             @click.prevent="register"
           >
             Добавить
           </button>
+          <div v-if="isFailure" class="text-danger">{{ error }}</div>
+          <div v-if="isSuccess" class="text-success">
+            Пользователь {{ username.value }} добавлен
+          </div>
         </form>
       </div>
     </div>
   </div>
 </template>
 
-<script lang="ts" setup></script>
+<script lang="ts" setup>
+import { key } from "@/_store";
+import { RegisterStatus } from "@/_store/register.module";
+import { computed } from "@vue/runtime-core";
+import { ref, Ref } from "vue";
+import { useStore } from "vuex";
+
+const store = useStore(key);
+
+const isSending = computed(
+  () => store.state.register?.status === RegisterStatus.sending
+);
+const isSuccess = computed(
+  () => store.state.register?.status === RegisterStatus.success
+);
+const isFailure = computed(
+  () => store.state.register?.status === RegisterStatus.failure
+);
+
+const error = computed(() => store.state.register?.error);
+
+const username = ref("");
+const password = ref("");
+const repeat = ref("");
+const admin = ref(false);
+const photo: Ref = ref(null);
+
+const register = async () => {
+  const fr = new FileReader();
+  fr.onload = (e: ProgressEvent<FileReader>) => {
+    store.dispatch("adminRegister", {
+      username: username.value,
+      password: password.value,
+      repeat: repeat.value,
+      admin: admin.value,
+      photo: e.target?.result,
+    });
+  };
+
+  try {
+    fr.readAsArrayBuffer(photo.value.files[0]);
+  } catch (e) {
+    console.error("file is not selected");
+    store.dispatch("setError", "необходимо выбрать файл");
+  }
+};
+</script>
