2433f646d8bb230128beb8ab1295ea60bdbc8b92
Alexey Plenkin <aveplen@yandex.ru>
2022-06-14 01:21:47 +0300

    add facerec distance regex check

--- src/_services/config.ts
+++ src/_services/config.ts
@@ -9,6 +9,10 @@ export const configService = {
 async function updateFacerecConfig(
   req: UpdateFacerecConfigRequest
 ): Promise<void> {
+  if (!/0\.0+[1-9][0-9]*/.test(req.distanceStr)) {
+    throw "необходимо число меньше единицы";
+  }
+
   try {
     await axios.post(`${config.baseUrl}/config/encr/facerec`, {
       distance: parseFloat(req.distanceStr),
